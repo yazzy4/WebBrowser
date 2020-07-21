cc59422be8410f6e21cd263ba964ec72b8301452
yazzy4 <yazburrell@pursuit.org>
2020-07-21 15:29:20 -0400

    Set up webview with google as landing page

--- WebBrowser/ViewController.swift
+++ WebBrowser/ViewController.swift
@@ -7,11 +7,25 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let url = URL(string: "https://www.google.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
       
     }
 
