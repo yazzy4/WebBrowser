24d1b8a00221756babe72542da2dd9bb6a621322
yazzy4 <yazburrell@pursuit.org>
2020-07-22 14:09:45 -0400

    added a progress bar to tool bar

--- WebBrowser/ViewController.swift
+++ WebBrowser/ViewController.swift
@@ -12,6 +12,7 @@ import WebKit
 class ViewController: UIViewController, WKNavigationDelegate {
     
     var webView: WKWebView!
+    var progressView: UIProgressView!
     
     override func loadView() {
         webView = WKWebView()
@@ -24,6 +25,15 @@ class ViewController: UIViewController, WKNavigationDelegate {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView)
+        
+        toolbarItems = [progressButton,spacer, refresh]
+        navigationController?.isToolbarHidden = false
         
         let url = URL(string: "https://www.google.com")!
         webView.load(URLRequest(url: url))
